c4f9e570f627bac5cae1e76bdc19e1f1b5c864e2
Erik <eahoff@gmail.com>
2025-09-30 15:51:45 +0200

    oving 5

---
+++ oving5/main.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <memory>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class ChessBoard {
+public:
+  enum class Color { WHITE,
+                     BLACK };
+
+  class Piece {
+  public:
+    Piece(Color color) : color(color) {}
+    virtual ~Piece() {}
+
+    Color color;
+    std::string color_string() const {
+      if (color == Color::WHITE)
+        return "white";
+      else
+        return "black";
+    }
+    std::string short_color_string() const {
+      if (color == Color::WHITE)
+        return "w";
+      else
+        return "b";
+    }
+    
+    virtual string short_type() const = 0;
+
+    /// Return color and type of the chess piece
+    virtual std::string type() const = 0;
+
+    /// Returns true if the given chess piece move is valid
+    virtual bool valid_move(int from_x, int from_y, int to_x, int to_y) const = 0;
+  };
+
+  class King : public Piece {
+  public:
+    King(Color color) : Piece(color) {}
+    std::string type() const override { return color_string() + " king"; }
+    std::string short_type() const override { return short_color_string() + "Ki"; }
+    bool valid_move(int from_x, int from_y, int to_x, int to_y) const override {
+      int dx = abs(from_x - to_x);
+      int dy = abs(from_y - to_y);
+      return dx <= 1 && dy <= 1 && (dx != 0 || dy != 0);
+    }
+  };
+
+  class Knight : public Piece {
+  public:
+    Knight(Color color) : Piece(color) {}
+    std::string type() const override { return color_string() + " knight"; }
+    std::string short_type() const override { return short_color_string() + "Kn"; }
+    bool valid_move(int from_x, int from_y, int to_x, int to_y) const override {
+      int dx = abs(from_x - to_x);
+      int dy = abs(from_y - to_y);
+      return (dx == 1 && dy == 2) || (dx == 2 && dy == 1);
+    }
+  };
+
+  ChessBoard() {
+    // Initialize the squares stored in 8 columns and 8 rows:
+    squares.resize(8);
+    for (auto &square_column : squares)
+      square_column.resize(8);
+  }
+
+  /// 8x8 squares occupied by 1 or 0 chess pieces
+  vector<vector<unique_ptr<Piece>>> squares;
+  
+  
+  void show() {
+    ostringstream oss;
+    
+    // enumeration //
+    // for (auto it = squares.begin(); it != squares.end(); ++it)
+    //   foo(*it, it - squares.begin());
+    
+    // first ln
+    for (auto it = squares.begin(); it != squares.end(); ++it) {
+      oss << "+---";
+    }
+    oss << "+" << endl;
+    
+    // content
+    for (int row = squares[0].size() - 1; row >= 0; --row) {
+        oss << "|";
+        for (size_t col = 0; col < squares.size(); ++col) {
+            string cell = "";
+            if (squares[col][row]) {
+                cell = squares[col][row]->short_type();
+            }
+            oss << setw(3) << cell << "|";
+        }
+        oss << endl;
+    
+        // horizontal separator
+        for (size_t col = 0; col < squares.size(); ++col) {
+            oss << "+---";
+        }
+        oss << "+" << endl;
+    }
+
+    cout << oss.str();
+  } 
+
+  /// Move a chess piece if it is a valid move.
+  /// Does not test for check or checkmate.
+  bool move_piece(const std::string &from, const std::string &to) {
+    int from_x = from[0] - 'a';
+    int from_y = stoi(string() + from[1]) - 1;
+    int to_x = to[0] - 'a';
+    int to_y = stoi(string() + to[1]) - 1;
+
+    auto &piece_from = squares[from_x][from_y];
+    if (piece_from) {
+      if (piece_from->valid_move(from_x, from_y, to_x, to_y)) {
+        cout << piece_from->type() << " is moving from " << from << " to " << to << endl;
+        auto &piece_to = squares[to_x][to_y];
+        if (piece_to) {
+          if (piece_from->color != piece_to->color) {
+            cout << piece_to->type() << " is being removed from " << to << endl;
+            if (auto king = dynamic_cast<King *>(piece_to.get()))
+              cout << king->color_string() << " lost the game" << endl;
+          } else {
+            // piece in the from square has the same color as the piece in the to square
+            cout << "can not move " << piece_from->type() << " from " << from << " to " << to << endl;
+            return false;
+          }
+        }
+        piece_to = move(piece_from);
+        show();
+        return true;
+      } else {
+        cout << "can not move " << piece_from->type() << " from " << from << " to " << to << endl;
+        return false;
+      }
+    } else {
+      cout << "no piece at " << from << endl;
+      return false;
+    }
+  }
+};
+
+int main() {
+  ChessBoard board;
+
+  board.squares[4][0] = make_unique<ChessBoard::King>(ChessBoard::Color::WHITE);
+  board.squares[1][0] = make_unique<ChessBoard::Knight>(ChessBoard::Color::WHITE);
+  board.squares[6][0] = make_unique<ChessBoard::Knight>(ChessBoard::Color::WHITE);
+
+  board.squares[4][7] = make_unique<ChessBoard::King>(ChessBoard::Color::BLACK);
+  board.squares[1][7] = make_unique<ChessBoard::Knight>(ChessBoard::Color::BLACK);
+  board.squares[6][7] = make_unique<ChessBoard::Knight>(ChessBoard::Color::BLACK);
+
+  cout << "Invalid moves:" << endl;
+  board.move_piece("e3", "e2");
+  board.move_piece("e1", "e3");
+  board.move_piece("b1", "b2");
+  cout << endl;
+
+  cout << "A simulated game:" << endl;
+  board.move_piece("e1", "e2");
+  board.move_piece("g8", "h6");
+  board.move_piece("b1", "c3");
+  board.move_piece("h6", "g8");
+  board.move_piece("c3", "d5");
+  board.move_piece("g8", "h6");
+  board.move_piece("d5", "f6");
+  board.move_piece("h6", "g8");
+  board.move_piece("f6", "e8");
+}
